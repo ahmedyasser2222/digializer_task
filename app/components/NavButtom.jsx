0a903d8bf6c185d5af20227c6b4eff07439c767e
Ahmed Yasser <ah01211293047@gmail.com>
2023-08-21 20:44:11 +0300

    Second Update

--- app/components/NavButtom.jsx
+++ app/components/NavButtom.jsx
@@ -6,23 +6,23 @@ import ButtonSearchMobile from "./ButtonSearchMobile"
 
 const NavButtom = () => {
   return (
-    <div className="flex md:hidden justify-evenly fixed bottom-0 left-0 w-full  bg-white border z-20">
+    <div className="flex md:hidden justify-evenly fixed bottom-0 left-0 w-full h-14 bg-white border z-20">
       <ButtonSearchMobile />
       {/* <button className='border w-2/5 btn-nav-get' >
                Search
           </button> */}
       <div className="flex justify-between w-1/2">
         <button className="w-1/4">
-          <BsPencilSquare size={45} className="w-full text-white py-2 icon-pencil" />
+          <BsPencilSquare size={55} className="w-full text-white py-3 icon-pencil" />
         </button>
         <button className="w-1/4">
-          <AiOutlinePhone size={45} className="w-full text-white py-2 icon-phone" />
+          <AiOutlinePhone size={55} className="w-full text-white py-3 icon-phone" />
         </button>
         <button className="w-1/4">
-          <BsWhatsapp size={45} className="w-full text-white  py-2 icon-whats" />
+          <BsWhatsapp size={55} className="w-full text-white  py-3 icon-whats" />
         </button>
         <button className="w-1/4">
-          <FaTelegramPlane size={45} className="w-full text-white py-2 icon-telegeam" />
+          <FaTelegramPlane size={55} className="w-full text-white py-3 icon-telegeam" />
         </button>
       </div>
     </div>
